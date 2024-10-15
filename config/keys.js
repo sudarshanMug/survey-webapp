04a6e372b9bb3cec865aca89e6367b0f5d5d64c0
sudarshanMug <sudarshanmugiti@gmail.com>
2024-10-15 12:29:44 +0530

    First commit

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// keys.js - figure out the credentials
+
+if(process.env .NODE_ENV === "production"){
+    //we are in production - return prod set of keys
+    module.exports = require('./prod')
+}else{
+    //we are in dev - return dev set of keys
+    module.exports = require('./dev')
+}
